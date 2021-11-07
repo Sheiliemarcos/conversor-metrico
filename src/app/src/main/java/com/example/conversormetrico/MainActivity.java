ac7b532a2aa21f866ca776bbdaf40a3de9b6b444
Sheiliemarcos Silva Ferreira <sheiliemarcos@tce.ro.gov.br>
2021-11-07 09:58:52 -0400

    Concluido a construção do app e adicionado aviso Toast!

--- src/app/src/main/java/com/example/conversormetrico/MainActivity.java
+++ src/app/src/main/java/com/example/conversormetrico/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.conversormetrico;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.btn_convert);
+        button.setOnClickListener(v -> {
+            EditText jardas = findViewById(R.id.input_jardas);
+            TextView result = (TextView)findViewById(R.id.txtViewResult);
+
+            if (jardas != null) {
+                double valor = Double.parseDouble(jardas.getText().toString());
+                valor = (valor / 1.094);
+                String message = String.format(Locale.getDefault(),"São %.2f metros.", valor );
+
+                result.setText(message);
+            } else {
+                CharSequence text = "Deve inserir um valor válido!";
+                int duration = Toast.LENGTH_SHORT;
+                Toast.makeText(getApplicationContext(), text, duration).show();
+            }
+        });
     }
 }
